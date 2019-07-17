3afdb89dd931125b792b894a88294b01b9cfb4a7
Nico Cooper <njcooper@princeton.edu>
2019-07-17 13:52:17 +0200

    Compiled test, added antikt function into regionizer

---
+++ python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/L1Trigger/Run3Ntuplizer/',1)[0])+'/cfipython/slc7_amd64_gcc700/L1Trigger/Run3Ntuplizer')
